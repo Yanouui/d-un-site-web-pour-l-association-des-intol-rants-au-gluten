754e9fc80dfd10d2e8e40b5896529a951d77c956
SOUMAAA <106883759+Yanouui@users.noreply.github.com>
2022-06-09 16:41:13 +0200

    Add files via upload

---
+++ source.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="source.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <style>
+        .A{
+            border: solid 5px  #274681;
+border-radius: 4%;
+        } 
+        .S{
+            margin-right: 12px;
+            font-size: 30px;
+        }.Se{
+            margin-right: 12px;
+            font-size: 26px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="topbutton">
+        <a href="#"><span class="fas fa-angle-up"> </span></a>
+
+    </div>
+
+    <div class="wrapper">
+
+        <div class="multi_color_border"></div>
+        <div class="top_nav">
+            <div class="left">
+
+
+            </div>
+            <div class="right">
+                <ul>
+               
+                    <li><a href="#Nous Contacter">Nous Contacter</a></li>
+                    <li><a href="#about">A propos de nous</a></li>
+                    <li><a href="signup.php">inscription</a></li>
+                  
+                </ul>
+            </div>
+        </div>
+    </div>
+    <header>
+        <div class="nav container">
+            <a href="Home1.php" class="logo"><i class='bx bxs-home-alt-2'></i>GlutenFree</a>
+            <input type="checkbox" name="" id="menu">
+            <label for="menu" <i class='bx bx-menu' id="menu-icon"></i> </label>
+            <ul class="navbar">
+                <li>
+                    <a href="">Les Maladies</a>
+
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href="cœliaque.php">Maladie coeliaque</a>
+                            </li>
+                            <li> <a href="sensibilite.php">Sensibilité AU Gluten</a></li>
+                            <li> <a href="herpétiforme.php">Dermatite herpétiforme</a></li>
+                        </ul>
+                    </div>
+
+
+
+
+                </li>
+                <li>
+                    <a href="">nos espace</a>
+
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href="Enfant.php">espace enfant</a>
+                            </li>
+                            <li> <a href="point.php">Point de vente</a></li>
+                            <li> <a href="produitt.php">les Produit </a></li>
+
+                        </ul>
+                    </div>
+
+
+
+
+                </li>
+                <li>
+                    <a href="#home">Le gluten </a>
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href="gulten.php">c'est quoi le gluten </a>
+                            </li>
+                            <li> <a href="source.php"> source du gluten</a></li>
+                          
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a href="#home">Regime</a>
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href="recette.php">Recette </a>
+                            </li>
+                            <li> <a href="Erreur.php">Au quotidien</a></li>
+
+                        </ul>
+                    </div>
+                </li>
+
+            </ul>
+
+
+
+        </div>
+
+
+
+
+
+
+    </header>
+
+
+        <section>
+<span class="image1">
+<img src="der.jpg" width="100%" height="400" alt="">
+</span>
+  
+<div class="first">
+    <h1>source du gluten</h1> <br><br>
+    <p>
+
+    De nombreux aliments peuvent contenir du gluten, souvent de manière cachée ou inattendue. Lisez toujours l'étiquette de
+    tout produit alimentaire que vous achetez si « sans gluten » n'est pas spécifié sur l'étiquette.
+    </p> <br> <br>
+
+    <h2>Céréales contenant du gluten et leurs dérivés </h2>
+</div>
+
+<span class="photo1">
+    <img src="ble.jpg" width="400" height="400 alt=" class="A"></span>
+<div class="blé">
+
+  <li> <h2 class="S"> Blé :</h2></li> <br> <br>
+  <li style=" font-size: 18px;"> Variétés et dérivés de blé tels que :</li><br>
+  <div class="hello
+  "> &nbsp; <li> 1-Grains de blé</li>
+   <li> 2-Blé dur</li>
+   <li> 3-emmerder</li>
+  <li>  4-La semoule</li>
+  <li>  5-Orthographié</li>
+   <li> 6-Farine</li>
+  <li>  7-Farro</li>
+   <li> 8-Graham</li>
+   <li> 9-Blé khorasan KAMUT®</li>
+  <li> 10-Petit épeautre</li> 
+  <li> 11-Seigle</li> 
+   <li> 12-Orge</li>
+   <li> 13-Triticale</li>
+   
+   </div>
+
+</div>
+
+
+<span class="photo1">
+    <img src="DDDD.webp" width="380" height="700 alt=" class="A"></span>
+<div class="blé">
+
+    <li> <h2 class="Se"> Aliments courants contenant du gluten :</h2>  </li>
+<br>
+  <li> <h4 style="color:#274681 ; " style=" font-size: 22px;">
+  1-Pâtes :
+  </h4> 
+  <li> 
+*Boulettes de raviolis couscous et gnocchis</li>  
+    
+   <li> <h4 style="color:#274681 ; " style=" font-size: 22px;">2-Pains et Viennoiseries :</h4>  <li>
+    *Croissants <br> *Pita <br> *Naan <br> *Bagels <br> *Pains plats <br> *Pain de maïs <br>
+     *Pain de pommes de terre <br> *Muffins <br> *Beignets <br> *petits pains</li></li>
+   <li> <h4 style="color:#274681 ; " style=" font-size: 22px;"> 3-Craquelins :</h4> <li> 
+    *Bretzels <br> *Poisson rouge <br> *Biscuits Graham</li></li>
+   <li> <h4 style="color:#274681 ; " style=" font-size: 22px;"> 4-Produits de boulangerie :</h4>  <li>
+    *Gâteaux <br> *Biscuits <br> *Pâtes à tarte <br> *Brownies</li></li> 
+    <li>
+   <li> <h4 style="color:#274681 ; " style=" font-size: 22px;">5-Aliments du petit-déjeuner : </h4><li>
+    *Crêpes <br> *Gaufres <br> *Pain doré <br>
+     *Crêpes et biscuits</li></li>
+  <li> <h4 style="color:#274681 ; " style=" font-size: 22px;"> 6-Mélanges pour panure et panure :</h4><li> 
+    *Chapelure panko</li> </li>
+  <li> <h4 style="color:#274681 ; " style=" font-size: 22px;"> 7-Croûtons:</h4> <li> 
+    *Farces <br> *Vinaigrettes </li> </li>
+
+</div>
+
+<span class="photo1">
+    <img src="jus.jpg" width="380" height="450 alt="  class="A"></span>
+<div class="blé">
+
+    <li> <h1
+     style="color:#274681 ; " style=" font-size: 35px;"> Boissons distillées et vinaigres</h1> </li> <br>
+    
+  <p class="para">
+
+La plupart des boissons distillées et des vinaigres sont sans gluten. <br> Ces produits distillés ne contiennent aucun
+peptide de gluten nocif, même s'ils sont fabriqués à partir de céréales contenant du gluten. <br> La recherche indique que le
+peptide de gluten est trop gros pour être transféré dans le processus de distillation, laissant le liquide résultant
+sans gluten. <br> Cependant, certains types de boissons alcoolisées contiennent une quantité dangereuse de gluten pour les
+personnes atteintes de la maladie cœliaque, et comprennent celles avec une couleur ou un arôme ajouté comme les vins de
+dessert et celles à base de malt d'orge, comme les refroidisseurs de vin en bouteille. Pour ceux-ci, les consommateurs
+doivent vérifier l'étiquette et, en cas de doute, contacter l'entreprise. , ales, lagers, boissons maltées et vinaigres
+de malt fabriqués à partir de grains contenant du gluten ne sont pas distillés et ne sont donc pas sans gluten.
+
+  </p>
+
+</div>
+
+
+
+        </section>
+        <!--footerrrrrrr-->
+        <section class="footer">
+            <br><br> <br>
+            <div class="footer-container container">
+                <h2>GlutenFree</h2>
+                <div class="footer-box">
+                    <h3>lien</h3>
+        
+                    <a href="cœliaque.php">Maladie coeliaque </a>
+                    <a href="gulten.php">c'est quoi le gluten </a>
+                    <a href="recette.php"> Recette</a>
+        
+                </div>
+        
+                <div class="footer-box">
+                    <h3>location</h3>
+                    <a href="#"> ALger </a>
+                    <a href="#">Annaba</a>
+                    <a href="#">Oran</a>
+        
+        
+                </div>
+        
+        
+                <div class="footer-box">
+                    <h3>Contact</h3>
+                    <a href="#">+213 (0)987 665 6543</a>
+                    <a href="#">cvbn@gmail.com</a>
+                    <div class="social">
+                        <a href="#"><i class='bx bxl-facebook'></i></a>
+                        <a href="#"><i class='bx bxl-instagram'></i></a>
+                        <a href="#"><i class='bx bxl-twitter'></i></a>
+        
+        
+                    </div>
+        
+        
+                </div>
+            </div>
+        
+        </section>
+
+</body>
+</html>
